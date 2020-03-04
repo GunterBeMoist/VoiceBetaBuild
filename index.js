90c78c3e82ec743b5a7049fd09d7e3022c018797
GunterBeMoist <61493427+GunterBeMoist@users.noreply.github.com>
2020-03-04 12:45:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ function myFunction() {
 
 window.SpeechRecognition = window.webkitSpeechRecognition || 
 window.SpeechRecognition;
+
+if ('SpeechRecognition' in window) {
+  // speech recognition API supported
+} else {
+  // speech recognition API not supported
+}
+
+const recognition = new window.SpeechRecognition();
+
+var myGrammars = mySpeechRecognition.grammars;
+mySpeechRecognition.grammars = mySpeechGrammarList;
+
+var myLang = mySpeechRecognition.lang;
+mySpeechRecognition.lang = 'en-US';
+
+var myInterimResult = mySpeechRecognition.interimResults;
+mySpeechRecognition.interimResults = false;
+
+var myMaxAlternativeNumber = mySpeechRecognition.maxAlternatives;
+mySpeechRecognition.maxAlternatives = 2;
+
+mySpeechRecognition.start();
+
+recognition.stop()
